0fc70066872a387cc72a1237d282d27dcc9380d5
Alexey Abdulin <alexexiv@gmail.com>
2020-12-10 20:01:15 +0300

    Test

--- app/src/main/java/com/speakerboxlite/rxentity/Entity.kt
+++ app/src/main/java/com/speakerboxlite/rxentity/Entity.kt
@@ -8,3 +8,20 @@ interface Entity<Key: Comparable<Key>>
 typealias EntityInt = Entity<Int>
 typealias EntityLong = Entity<Long>
 typealias EntityString = Entity<String>
+
+interface EntityBack<Key: Comparable<Key>>
+{
+    val _key: Key
+}
+
+typealias EntityBackInt = Entity<Int>
+typealias EntityBackLong = Entity<Long>
+typealias EntityBackString = Entity<String>
+
+interface EntityBackToEntityMapper<Key: Comparable<Key>, Source: EntityBack<Key>, Dest: Entity<Key>>
+{
+    fun map(entity: Source): Dest
+    {
+        throw IllegalArgumentException("${entity::class} is not convertible to ${this::class}")
+    }
+}
